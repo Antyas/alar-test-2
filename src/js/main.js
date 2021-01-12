6eb5f608d62b20ce40ec7f23549bcb82119645ae
Roman Antyaskin <roman.antyaskin@maximumtest.ru>
2021-01-12 13:45:36 +0300

    ожидание загрузки дома

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,7 @@ import App from './app';
 import components from './components/index.js';
 import modules from './modules/index.js';
 
-const app = new App(modules, components);
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new App(modules, components);
+});
+
